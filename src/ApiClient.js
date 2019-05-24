c9f05ab9a381cda338e5b1f3ba551e9dcac4a76c
Adam Grohs <adamgrohs@Adams-MacBook-Air-2.local>
2019-05-23 22:43:27 -0500

    fix: changed logic in case oauth2AccessToken as well as autogen oauth

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -114,7 +114,9 @@ export default class ApiClient {
 
         if (oauth2AccessToken) {
             this.authentications.oauth2_application.accessToken = oauth2AccessToken
-        } else if (clientId && clientSecret && clientUsername && clientPassword) {
+        }
+        
+        if (clientId && clientSecret && clientUsername && clientPassword) {
             this.authentications.oauth2_application.accessCredentials = {
                 clientId,
                 clientSecret,
